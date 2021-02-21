8f104ad75246b1847bee91f7a686f137edec16ed
Josh Ghent <me@joshghent.com>
2021-02-21 18:00:19 +0000

    Added try catch

--- server/src/lib/resize.ts
+++ server/src/lib/resize.ts
@@ -3,12 +3,17 @@ import sharp from 'sharp';
 
 export class Image {
   public static resize(path: string, format?: string, width?: number, height?: number) {
-    const readStream = fs.createReadStream(path);
-    let transform = sharp();
-    if (width || height) {
-      transform = transform.resize(width, height);
+    try {
+      const readStream = fs.createReadStream(path);
+      let transform = sharp();
+      if (width || height) {
+        transform = transform.resize(width, height);
+      }
+      return readStream.pipe(transform);
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
-    return readStream.pipe(transform);
   };
 }
 
