444eae8022a213b8ae9dace3c9dbcd7cf3847d73
Josh Ghent <me@joshghent.com>
2021-02-21 16:44:12 +0000

    Updated app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>place.dogs</h1>
       </header>
+      <section className="main">
+        <p>A simple service to get cute dogs as placeholders for your websites and designs. Just add a width and height to the end of the url.</p>
+        <img src="https://dog.ghent.cloud/300/200" alt="Dog" />
+        <p>Try in your Browser</p>
+        <code>https://place.dog/300/200</code>
+        <p>Or, in your Terminal</p>
+        <code>curl -i https://place.dog/300/200 -o doggo.jpeg</code>
+      </section>
+      <footer>
+        <p>Built by <a href="https://joshghent.com">Josh Ghent</a> at <a href="https://turboapi.dev">turboapi.dev</a></p>
+      </footer>
     </div>
   );
 }
