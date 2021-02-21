5f36eddda7a879873267b8ee6caf4d68205e2bbf
Josh Ghent <me@joshghent.com>
2021-02-21 16:47:43 +0000

    Updated image url

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>place.dogs</h1>
+        <h1>place.dog</h1>
       </header>
       <section className="main">
         <p>A simple service to get cute dogs as placeholders for your websites and designs. Just add a width and height to the end of the url.</p>
-        <img src="https://dog.ghent.cloud/300/200" alt="Dog" />
+        <img src="https://place.dog/300/200" alt="Dog" />
         <p>Try in your Browser</p>
         <code>https://place.dog/300/200</code>
         <p>Or, in your Terminal</p>
