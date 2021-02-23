aec015f627d9c52813e2d39b3d5142b89a4d50ad
Josh Ghent <me@joshghent.com>
2021-02-23 13:07:30 +0000

    Added submit prompt

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ function App() {
         <h1>place.dog</h1>
       </header>
       <section className="main">
-        <p>A simple service to get cute dogs as placeholders for your websites and designs. Just add a width and height to the end of the url.</p>
+        <h2>A simple service to get cute dogs as placeholders for your websites and designs. Just add a width and height to the end of the url.</h2>
         <img src="https://place.dog/300/200" alt="Dog" />
         <p>Try in your Browser</p>
         <code>https://place.dog/300/200</code>
         <p>Or, in your Terminal</p>
         <code>curl -i https://place.dog/300/200 -o doggo.jpeg</code>
+
+        <div className="submit-prompt">
+          <p>Got a cute doggo? Submit yours to <a href="mailto:josh@turboapi.dev">josh@turboapi.dev</a> or <a href="https://twitter.com/joshghent">@joshghent</a> on Twitter</p>
+        </div>
       </section>
       <footer>
         <p>Built by <a href="https://joshghent.com">Josh Ghent</a> at <a href="https://turboapi.dev">turboapi.dev</a></p>
