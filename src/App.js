156450a0136bc8cb0812001f644318254a32e684
Josh Ghent <me@joshghent.com>
2023-05-09 10:54:24 +0100

    added fly and more images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,10 @@ function App() {
         <h1>place.dog</h1>
       </header>
       <section className="main">
-        <h2>A simple service to get cute dogs as placeholders for your websites and designs. Just add a width and height to the end of the url.</h2>
+        <h2>
+          A simple service to get cute dogs as placeholders for your websites
+          and designs. Just add a width and height to the end of the url.
+        </h2>
         <img src="https://place.dog/300/200" alt="Dog" />
         <p>Try in your Browser</p>
         <code>https://place.dog/300/200</code>
@@ -15,11 +18,19 @@ function App() {
         <code>curl -i https://place.dog/300/200 -o doggo.jpeg</code>
 
         <div className="submit-prompt">
-          <p>Got a cute doggo? Submit yours to <a href="mailto:josh@turboapi.dev">josh@turboapi.dev</a> or <a href="https://twitter.com/joshghent">@joshghent</a> on Twitter</p>
+          <p>
+            Got a cute doggo? Submit yours to{" "}
+            <a href="https://twitter.com/joshghent">@joshghent</a> on Twitter
+          </p>
         </div>
       </section>
       <footer>
-        <p>Built by <a href="https://joshghent.com">Josh Ghent</a> at <a href="https://turboapi.dev">turboapi.dev</a></p>
+        <p>
+          Built by <a href="https://joshghent.com">Josh Ghent</a> at{" "}
+          <a href="https://loginllama.app">
+            LoginLlama - Suspicious Login Detection powered by AI.
+          </a>
+        </p>
       </footer>
     </div>
   );
